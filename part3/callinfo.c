4d29c9af0471bfe04d631eaebfbb7b870500d482
Dorvin <ladig1017@gmail.com>
2020-04-07 23:02:23 +0900

    part3 third modify

--- part3/callinfo.c
+++ part3/callinfo.c
@@ -8,18 +8,20 @@ int get_callinfo(char *fname, size_t fnlen, unsigned long long *ofs)
   unw_word_t offset;
   unw_word_t pc;
   if (unw_getcontext(&context))
-    return;
+    return -1;
   if (unw_init_local(&cursor, &context))
-    return;
+    return -1;
   while (unw_step(&cursor) > 0) {
     unw_get_reg(&cursor, UNW_REG_IP, &pc);
     if (pc == 0) {
       break;
     }
     //printf("0x%lx:", pc);
-    if (unw_get_proc_name(&cursor, fname, fnlen, &ofs) == 0) {
-      if(fname[0] == 'm' && fname[1] == 'a' && fname[2] == 'i' && fname[3] == 'n');
-        return pc;
+    if (unw_get_proc_name(&cursor, fname, fnlen, &offset) == 0) {
+      if(fname[0] == 'm' && fname[1] == 'a' && fname[2] == 'i' && fname[3] == 'n'){
+        *ofs = (unsigned long long)offset;
+        return (int)pc;
+      }
       //printf(" (%s+0x%lx)\n", sym, offset);
     } else {
       //printf(" -- error: unable to obtain symbol name for this frame\n");
